38850d9b2e2d575a201b0570bfe45c86f25d1de2
Tanu Chaurasiya <tanuchaurasiya@Tanus-MacBook-Air.local>
2025-05-06 23:11:52 +0530

    router setup: video 16 completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
-import React, {useState} from 'react'
+import React, {useState} from 'react';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light');
@@ -13,33 +18,37 @@ function App() {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark mode has been enabled", "success");
-    }
-    else{
+      document.title = "Text utils - Dark mode";
+    } else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled", "success");
+      document.title = "Text utils - Light mode";
     }
   }
 
   const [alert, setAlert] = useState(null);
 
   const showAlert = (message, type)=>{
-      setAlert({
-        msg: message,
-        type: type
-      })
-      setTimeout(() => {
-          setAlert(null);
-      }, 1500);
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
   }
 
   return (
-    <>
-      <Navbar title="TextUtils" aboutText="AboutTextUtils" mode={mode} toggleMode={toggleMode}></Navbar>
-      <Alert alert={alert}/>
-      <TextForm heading="Enter text" mode={mode} showAlert={showAlert}></TextForm>
-      {/* <About></About> */}
-    </>
+    <Router>
+      <Navbar title="TextUtils" aboutText="AboutTextUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+
+      <Routes>
+        <Route path="/" element={<TextForm heading="Enter text" mode={mode} showAlert={showAlert} />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
